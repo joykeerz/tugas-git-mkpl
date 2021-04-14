7fbadd754f6a4884020bdc909a0e48ce7b12b7d1
haikalkamal <haikalk132@gmail.com>
2021-04-14 16:09:06 +0700

    MakananRingan

---
+++ app/Http/Controllers/MakananRinganController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class MakananRinganController extends Controller
+{
+    public function tampil()
+    {
+        $MakananRingan = DB::table('makanan_ringan')->select('*')->get();
+
+        return view('MakananRingan.MakananRingan', ['MakananRingan' => $MakananRingan]);
+    }
+
+    public function tambahData(Request $request)
+    {
+        DB::table('makanan_ringan')->insert([
+        'nama_makanan' => $request->namaMakananRingan,
+        'harga_makanan' => $request->hargaMakananRingan,
+        'jumlah_makanan' => $request->jumlahMakananRingan
+        ]);
+        return view('MakananRingan.MakananRingan');
+    }
+
+    public function editData($id)
+    {
+        $data = DB::table('makanan_ringan')->where('id',$id)->first();
+        return view('MakananRingan.edit',['MakananRingan'=>$data]);
+    }
+
+    public function updateData(Request $request, $id)
+    {
+        DB::table('makanan_ringan')->where('id',$id)->update([
+            'nama_makanan' => $request -> namaMakananRingan,
+            'harga_makanan' => $request -> hargaMakananRingan,
+            'jumlah_makanan' => $request -> jumlahMakananRingan,    
+            ]);
+
+        $data = DB::table('makanan_ringan') -> select ('*') -> get();
+        return view('MakananRingan.MakananRingan', ['MakananRingan' => $data ]);
+    }
+
+    public function hapusData($id)
+    {
+        DB::table('makanan_ringan')->where('id',$id)->delete();
+        $data=DB::table('makanan_ringan')->select('*')->get();
+        return view('MakananRingan.MakananRingan', ['MakananRingan'=>$data]);
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/MakananRingan', function () {
+//     return view('MakananRingan.MakananRingan');
+// });
+
+Route::get('/MakananRingan', 'MakananRinganController@tampil');
+Route::post('/MakananRingan/tambah','MakananRinganController@tambahData');
+Route::get('/MakananRingan/edit/{id}','MakananRinganController@editData');
+Route::post('/MakananRingan/update/{id}','MakananRinganController@updateData');
+Route::get('/MakananRingan/hapus/{id}','MakananRinganController@hapusData');
