c7bb0d59d365bcb13a593fb507019c75ad688d41
stpangestu <saktitripangestu3291@gmail.com>
2021-04-22 00:12:47 +0700

    rubah tampilan home

--- resources/views/minuman_dingin/minumanDingin.blade.php
+++ resources/views/minuman_dingin/minumanDingin.blade.php
@@ -19,7 +19,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Takjil</a>
@@ -31,7 +31,7 @@
               <a class="nav-link" href="#">Makanan ringan</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Minuman dingin</a>
+              <a class="nav-link" href="/minumandingin">Minuman dingin</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Minuman hangat</a>
