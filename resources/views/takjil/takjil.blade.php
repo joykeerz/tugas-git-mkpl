f022a3b546754d02732130efe5b5ab439d7af65a
wijoyo wisnu mukti <wisnu.mukti10@gmail.com>
2021-04-22 13:56:58 +0700

    fixed navbar link for takjil module and home

--- resources/views/takjil/takjil.blade.php
+++ resources/views/takjil/takjil.blade.php
@@ -19,10 +19,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-              <a class="nav-link" href="#">Home </a>
+              <a class="nav-link" href="/">Home</a>
             </li>
             <li class="nav-item active">
-              <a class="nav-link" href="#">Takjil</a>
+              <a class="nav-link" href="/takjil">Takjil</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Makanan berat</a>
